0df2fd38ba7913c29069af820cbe8d30bd9f139d
AyaPK <Sherbnya@gmail.com>
2025-07-17 00:20:03 +0100

    Adds idle shoot state for player
    Adds the idle shoot state to the player's state machine,
    allowing the player to shoot while in the idle state.
    
    Includes an animation for the idle shoot action and
    updates the player script to handle exiting the shoot state.

--- player/scripts/player.gd
+++ player/scripts/player.gd
@@ -7,6 +7,12 @@ class_name Player extends CharacterBody2D
 
 var on_ladder: bool = false
 var v: Vector2 = Vector2.ZERO
+var anim_frame: int = 0
+@onready var idle: State_Idle = $StateMachine/Idle
+
+var state_mapping: Dictionary = {
+	"idle": idle
+}
 
 func _ready() -> void:
 	state_machine.initialise(self)
@@ -28,3 +34,8 @@ func _on_hitbox_body_entered(_body: Node2D) -> void:
 
 func _on_hitbox_body_exited(_body: Node2D) -> void:
 	on_ladder = false
+
+func leave_shoot() -> void:
+	var anim_name: String = animation_player.current_animation.replace("_shoot", "")
+	animation_player.play(anim_name)
+	state_machine.changeState(state_mapping[anim_name])
--- player/scripts/states/idle_shoot.gd
+++ player/scripts/states/idle_shoot.gd
@@ -1,25 +1,21 @@
-class_name State_Idle extends State
+class_name State_Idle_Shoot extends State
 
 @onready var run: State_Run = $"../Run"
 @onready var jump: State_Jump = $"../Jump"
 @onready var fall: State_Fall = $"../Fall"
 @onready var climb_up: State_Climb_Up = $"../ClimbUp"
 @onready var climb_down: State_Climb_Down = $"../ClimbDown"
-@onready var idle_shoot: State_Idle_Shoot = $"../IdleShoot"
 
 func _ready() -> void:
 	pass
 
 func enter() -> void:
-	player.update_animation("idle")
+	player.update_animation("idle_shoot")
 
 func exit() -> void:
 	pass
 
 func process(_delta: float) -> State:
-	player.velocity = Vector2.ZERO
-	if !player.is_on_floor():
-		return fall
 	return null
 
 func physics(_delta: float) -> State:
@@ -28,12 +24,10 @@ func physics(_delta: float) -> State:
 func handle_input(_event: InputEvent) -> State:
 	if Input.is_action_pressed("move_right") or Input.is_action_pressed("move_left"):
 		return run
-	elif Input.is_action_just_pressed("jump"):
+	if Input.is_action_just_pressed("jump"):
 		return jump
-	elif Input.is_action_just_pressed("move_up") and player.on_ladder:
+	if Input.is_action_just_pressed("move_up") and player.on_ladder:
 		return climb_up
-	elif Input.is_action_just_pressed("move_down") and player.on_ladder:
+	if Input.is_action_just_pressed("move_down") and player.on_ladder:
 		return climb_down
-	elif Input.is_action_just_pressed("shoot"):
-		return idle_shoot
 	return null
