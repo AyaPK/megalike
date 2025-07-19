4c928c926dfbdef1222d5dee534849b1df669cc1
AyaPK <Sherbnya@gmail.com>
2025-07-19 13:33:41 +0100

    Add screen transition nodes
    Placing screen transition nodes in the editor will clamp the camera's horiztonal movement by using onscreen notifiers

---
+++ nodes/scripts/screen_transition.gd
@@ -0,0 +1,23 @@
+class_name Screen_Transition extends Node2D
+
+@onready var camera: Camera2D = $"../Camera"
+
+enum Sides {LEFT, RIGHT}
+@export var side: Sides
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_visible_on_screen_notifier_2d_screen_entered() -> void:
+	print("entered")
+	if side == Sides.LEFT:
+		camera.limit_left = global_position.x
+	else:
+		camera.limit_right = global_position.x
